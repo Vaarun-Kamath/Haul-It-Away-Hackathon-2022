8b1bfe1b328faa70d2e5b0644a9faced63d5314c
VarunKamath <varunkamath05@gmail.com>
2022-09-24 11:58:05 +0530

    half done/vikas test

--- FitBitApp/app/src/main/java/com/example/fitbitproject/MainActivity.java
+++ FitBitApp/app/src/main/java/com/example/fitbitproject/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.fitbitproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    RelativeLayout relativeLayout;
+    TextView textView;
+    SwipeListener swipeListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        relativeLayout = findViewById(R.id.relative_layout);
+        textView = findViewById(R.id.text_view);
+    }
+
+    private class SwipeListener implements View.OnTouchListener {
+
+        GestureDetector gestureDetector;
+        SwipeListener(View view){
+            int threshold = 100;
+            int velocity_threshold = 100;
+            GestureDetector.SimpleOnGestureListener listener =
+                    new GestureDetector.SimpleOnGestureListener(){
+                        @Override
+                        public boolean onDown(MotionEvent e) {
+                            return true;
+                        }
+
+                        @Override
+                        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                            float xDiff = e2.getX() - e1.getX();
+                            float yDiff = e2.getY() - e1.getY();
+                        }
+                    };
+        }
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            return false;
+        }
     }
 }
