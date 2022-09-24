f7e9a368b94cd874a7973603b93b9738dae182cd
RockerBot <rockerbotmail@gmail.com>
2022-09-25 02:59:15 +0530

    can click on table now

--- StudioApplication/app/src/main/java/com/example/studioapplication/Wordle.java
+++ StudioApplication/app/src/main/java/com/example/studioapplication/Wordle.java
@@ -1,8 +1,13 @@
 package com.example.studioapplication;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 public class Wordle extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class Wordle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wordle);
+        TableLayout a = (TableLayout)findViewById(R.id.wordle_tbL1);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("Click","asdfgh");
+            }
+        });
     }
+
 }
