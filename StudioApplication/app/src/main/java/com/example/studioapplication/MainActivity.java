5b7d026b9b7ed610cd4ba45eb7768719610d1c6d
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-09-25 03:57:07 +0530

    Squashed commit of the following:
    commit f30a8e2bfc7641694e981d31ad01db1ebb0122ba
    Merge: 0e742dd 875cbfe
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 03:52:40 2022 +0530
    
        Merge branch 'wordle'
    
    commit 875cbfefee6beec9be51fc647101e30ca233ad52
    Merge: f2cade9 01f35a4
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 03:51:58 2022 +0530
    
        Merge branch 'vault' into wordle
    
    commit 01f35a41a9b8b015d135c9ff558f92ee98124ff2
    Author: VarunKamath <varunkamath05@gmail.com>
    Date:   Sun Sep 25 03:45:13 2022 +0530
    
        Vault Prototype NOT DONE DRIVE
    
    commit f2cade96f71c2089f89d997829297faf043faf94
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 03:33:50 2022 +0530
    
        Changed String Resources
    
    commit 9b9608cc4f48852de784f080619fa5e33165431d
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 03:32:34 2022 +0530
    
        i got keyboard
    
    commit f7e9a368b94cd874a7973603b93b9738dae182cd
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 02:59:15 2022 +0530
    
        can click on table now
    
    commit 4187daa35695b237203826dc984a0c3784f224bc
    Author: VarunKamath <varunkamath05@gmail.com>
    Date:   Sun Sep 25 01:37:40 2022 +0530
    
        main app home page prototype
    
    commit be99587f575a4a66b702dc40268d9e3fdf0ba930
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 21:37:44 2022 +0530
    
        Added color, vertical spacing and dimen resource
    
    commit e19022db8c16b62c1331d48e006c22ce328dc40b
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 19:48:55 2022 +0530
    
        Aligned wordle letters horizontally
    
    commit 6e50d80fb60b14a26e5c23d2968367ebecdc38dc
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sat Sep 24 18:47:32 2022 +0530
    
        added slots for wordle
    
    commit 7e8d5898044439f4e453ef46248edab6b26dc1b4
    Merge: 68bde01 c9bc6be
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 17:16:01 2022 +0530
    
        Merge branch 'wordle' of https://github.com/cubed-guy/fit-bytes into wordle
    
    commit 68bde01ce1da2db1cac062be6aea0f93b29f74ca
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 17:15:40 2022 +0530
    
        Added fitbit to wordle
    
    commit c9bc6be253c958a4602a941cebc51740fddcc6f0
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sat Sep 24 17:10:36 2022 +0530
    
        changed start activity
    
    commit 3898c7e4009893694c8b7035176b06abeebd148a
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 14:12:45 2022 +0530
    
        organized stuff
    
    commit 2ade7ceeb3cb7747469c08b8abf990f507a605ef
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 12:38:20 2022 +0530
    
        Progress on Basic Page
    
    commit 1ce6ed0334556ef195d3c22637ab77eec615e6e7
    Merge: 3d9210d 8a29d97
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 12:20:37 2022 +0530
    
        Sorta Aligned the text fields
    
    commit 3d9210dda115f502fd8199d89d7b8961c7f419cc
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 11:55:22 2022 +0530
    
        Deleted StudioProject
    
    commit 8a29d97dffa49557b9dc66a9335aed2f487f4316
    Author: thak-gan <thaksha.ganesh@gmail.com>
    Date:   Sat Sep 24 11:54:42 2022 +0530
    
        wordle second
    
    commit f2cd2b2b32986004ced1b170cd63ac23c4a73932
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 11:35:19 2022 +0530
    
        Initialized Wordle
    
    commit 8783ad86ecefe7b89446b9bdef5ae1d08ed3a0d9
    Author: thak-gan <thaksha.ganesh@gmail.com>
    Date:   Sat Sep 24 11:29:08 2022 +0530
    
        Wordle_first

--- StudioApplication/app/src/main/java/com/example/studioapplication/MainActivity.java
+++ StudioApplication/app/src/main/java/com/example/studioapplication/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.studioapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+//import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.studioapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_main); TODO IDK WHAT HE DID BUT HE REMOVED THE PARAMETERS HERE I DIDNT WANT TO DELETE :)
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemReselectedListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case R.id.home_button:
+                    replaceFragment(new HomeFragment());
+//                    setContentView(R.layout.fragment_home);
+                    break;
+                case R.id.person_button:
+                    replaceFragment(new PersonFragment());
+//                    setContentView(R.layout.fragment_person);
+                    break;
+                case R.id.settings_button:
+                    replaceFragment(new SettingsFragment());
+//                    setContentView(R.layout.fragment_home);
+                    break;
+
+
+
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_id,fragment);
+        fragmentTransaction.commit();
+
     }
 }
---
+++ StudioApplication/app/src/main/java/com/example/studioapplication/MyMovieAdapter.java
@@ -0,0 +1,75 @@
+package com.example.studioapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class MyMovieAdapter extends RecyclerView.Adapter<MyMovieAdapter.ViewHolder> {
+
+    ArrayList<MyMovieData>myMovieData;
+    Context context;
+
+    public MyMovieAdapter(ArrayList<MyMovieData> myMovieData, MainActivity activity) {
+        this.myMovieData = myMovieData;
+        this.context = activity;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.video_list,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final MyMovieData myMovieDataList = myMovieData.get(position);
+//        holder.videoImage.setImageResource(myMovieDataList.getMovieImage());
+        holder.textViewDate.setText(myMovieDataList.getVideoDate());
+        holder.textViewTime.setText(myMovieDataList.getVideoTime());
+        holder.textViewLocation.setText(myMovieDataList.getVideoLocation());
+        holder.textViewHeartRate.setText(myMovieDataList.getVideoHeartRate());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"GG",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return myMovieData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+//        ImageView videoImage;
+        TextView textViewDate;
+        TextView textViewTime;
+        TextView textViewLocation;
+        TextView textViewHeartRate;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+//            videoImage = itemView.findViewById(R.id.imageview);
+            textViewDate = itemView.findViewById(R.id.dateview);
+            textViewTime = itemView.findViewById(R.id.timeview);
+            textViewLocation = itemView.findViewById(R.id.locationview);
+            textViewHeartRate = itemView.findViewById(R.id.heartview);
+        }
+    }
+}
