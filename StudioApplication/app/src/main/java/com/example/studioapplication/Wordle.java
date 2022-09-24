8ab92decea24e80c92b0f03acbbae60acf634f5d
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-09-25 03:54:49 +0530

    Squashed commit of the following:
    commit f30a8e2bfc7641694e981d31ad01db1ebb0122ba
    Merge: 0e742dd 875cbfe
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 03:52:40 2022 +0530
    
        Merge branch 'wordle'
    
    commit 875cbfefee6beec9be51fc647101e30ca233ad52
    Merge: f2cade9 01f35a4
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 03:51:58 2022 +0530
    
        Merge branch 'vault' into wordle
    
    commit f2cade96f71c2089f89d997829297faf043faf94
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 03:33:50 2022 +0530
    
        Changed String Resources
    
    commit 9b9608cc4f48852de784f080619fa5e33165431d
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 03:32:34 2022 +0530
    
        i got keyboard
    
    commit f7e9a368b94cd874a7973603b93b9738dae182cd
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 02:59:15 2022 +0530
    
        can click on table now
    
    commit be99587f575a4a66b702dc40268d9e3fdf0ba930
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 21:37:44 2022 +0530
    
        Added color, vertical spacing and dimen resource
    
    commit e19022db8c16b62c1331d48e006c22ce328dc40b
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 19:48:55 2022 +0530
    
        Aligned wordle letters horizontally
    
    commit 6e50d80fb60b14a26e5c23d2968367ebecdc38dc
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sat Sep 24 18:47:32 2022 +0530
    
        added slots for wordle
    
    commit 0e742dd268ddd72474a344e903fabd209e2dc3d7
    Author: VarunKamath <varunkamath05@gmail.com>
    Date:   Sat Sep 24 14:08:29 2022 +0530
    
        Fitbit_V1_Complete(For now :))
    
    commit 8b1bfe1b328faa70d2e5b0644a9faced63d5314c
    Author: VarunKamath <varunkamath05@gmail.com>
    Date:   Sat Sep 24 11:58:05 2022 +0530
    
        half done/vikas test
    
    commit e6866f14eaee5114624653b94fbc1d06ab733d4d
    Author: VarunKamath <varunkamath05@gmail.com>
    Date:   Sat Sep 24 11:28:42 2022 +0530
    
        Init FitBit

--- StudioApplication/app/src/main/java/com/example/studioapplication/Wordle.java
+++ StudioApplication/app/src/main/java/com/example/studioapplication/Wordle.java
@@ -1,14 +1,54 @@
 package com.example.studioapplication;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 public class Wordle extends AppCompatActivity {
+
+    int row_counter = 0, col_counter=0;
+    View[][] rows ={
+            {findViewById(R.id.wordle_11), findViewById(R.id.wordle_12), findViewById(R.id.wordle_13), findViewById(R.id.wordle_14), findViewById(R.id.wordle_15)},
+            {findViewById(R.id.wordle_21),findViewById(R.id.wordle_22),findViewById(R.id.wordle_23),findViewById(R.id.wordle_24),findViewById(R.id.wordle_25)},
+            {findViewById(R.id.wordle_31),findViewById(R.id.wordle_32),findViewById(R.id.wordle_33),findViewById(R.id.wordle_34),findViewById(R.id.wordle_35)},
+            {findViewById(R.id.wordle_41),findViewById(R.id.wordle_42),findViewById(R.id.wordle_43),findViewById(R.id.wordle_44),findViewById(R.id.wordle_45)},
+            {findViewById(R.id.wordle_51),findViewById(R.id.wordle_52),findViewById(R.id.wordle_53),findViewById(R.id.wordle_54),findViewById(R.id.wordle_55)},
+            {findViewById(R.id.wordle_61),findViewById(R.id.wordle_62),findViewById(R.id.wordle_63),findViewById(R.id.wordle_64),findViewById(R.id.wordle_65)}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wordle);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+        TableLayout a = (TableLayout)findViewById(R.id.wordle_tbL1);
+        EditText myEditText = (EditText)findViewById(R.id.wordle_et1);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(myEditText.requestFocus()) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+                }
+                Log.d("Click","asdfgh");
+            }
+        });
+        myEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // showMyDialog();
+            }
+        });
     }
+
 }
