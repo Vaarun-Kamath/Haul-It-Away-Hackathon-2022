a950a70877b2038141f22143d672641866e29d49
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-09-25 06:22:39 +0530

    Squashed commit of the following:
    commit 04d8ff8c64663694d78b98108893688b55762918
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 06:15:06 2022 +0530
    
        WordleCamo works
    
        but only with AIKYA
    
    commit f8d445bcb48bac13dfb65cd8f251ce34d7f3b425
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 05:24:31 2022 +0530
    
        sort of works
    
    commit b5c8105f01a5cf162551fd9747a58cc593849331
    Merge: 22d0a0a eb58427
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 04:46:24 2022 +0530
    
        Merge branch 'wordle' of https://github.com/cubed-guy/fit-bytes into wordle
    
    commit 22d0a0ae765d3c611a806975ea0369596a941cea
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 04:46:08 2022 +0530
    
        Added all words
    
    commit eb5842764d07f809f4d947d4f47fc86e66ad0723
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 04:14:32 2022 +0530
    
        steps  towards a wordle app
    
    commit 875cbfefee6beec9be51fc647101e30ca233ad52
    Merge: f2cade9 01f35a4
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 03:51:58 2022 +0530
    
        Merge branch 'vault' into wordle
    
    commit f2cade96f71c2089f89d997829297faf043faf94
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sun Sep 25 03:33:50 2022 +0530
    
        Changed String Resources
    
    commit 9b9608cc4f48852de784f080619fa5e33165431d
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 03:32:34 2022 +0530
    
        i got keyboard
    
    commit f7e9a368b94cd874a7973603b93b9738dae182cd
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sun Sep 25 02:59:15 2022 +0530
    
        can click on table now
    
    commit be99587f575a4a66b702dc40268d9e3fdf0ba930
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 21:37:44 2022 +0530
    
        Added color, vertical spacing and dimen resource
    
    commit e19022db8c16b62c1331d48e006c22ce328dc40b
    Author: Cubed <47624167+cubed-guy@users.noreply.github.com>
    Date:   Sat Sep 24 19:48:55 2022 +0530
    
        Aligned wordle letters horizontally
    
    commit 6e50d80fb60b14a26e5c23d2968367ebecdc38dc
    Author: RockerBot <rockerbotmail@gmail.com>
    Date:   Sat Sep 24 18:47:32 2022 +0530
    
        added slots for wordle

--- StudioApplication/app/src/main/java/com/example/studioapplication/Wordle.java
+++ StudioApplication/app/src/main/java/com/example/studioapplication/Wordle.java
@@ -1,21 +1,33 @@
 package com.example.studioapplication;
 
 
+
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
+
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TableLayout;
+
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class Wordle extends AppCompatActivity {
+import java.util.Locale;
 
+public class Wordle extends AppCompatActivity {
     int row_counter = 0, col_counter=0;
+
     View[][] rows ={
             {findViewById(R.id.wordle_11), findViewById(R.id.wordle_12), findViewById(R.id.wordle_13), findViewById(R.id.wordle_14), findViewById(R.id.wordle_15)},
             {findViewById(R.id.wordle_21),findViewById(R.id.wordle_22),findViewById(R.id.wordle_23),findViewById(R.id.wordle_24),findViewById(R.id.wordle_25)},
@@ -36,6 +48,8 @@ public class Wordle extends AppCompatActivity {
         a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                EditText myEditText = (EditText)findViewById(R.id.wordle_et1);
                 if(myEditText.requestFocus()) {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
@@ -43,10 +57,60 @@ public class Wordle extends AppCompatActivity {
                 Log.d("Click","asdfgh");
             }
         });
-        myEditText.setOnClickListener(new View.OnClickListener() {
+        View[][] wordls ={
+                {findViewById(R.id.wordle_11), findViewById(R.id.wordle_12), findViewById(R.id.wordle_13), findViewById(R.id.wordle_14), findViewById(R.id.wordle_15)},
+                {findViewById(R.id.wordle_21),findViewById(R.id.wordle_22),findViewById(R.id.wordle_23),findViewById(R.id.wordle_24),findViewById(R.id.wordle_25)},
+                {findViewById(R.id.wordle_31),findViewById(R.id.wordle_32),findViewById(R.id.wordle_33),findViewById(R.id.wordle_34),findViewById(R.id.wordle_35)},
+                {findViewById(R.id.wordle_41),findViewById(R.id.wordle_42),findViewById(R.id.wordle_43),findViewById(R.id.wordle_44),findViewById(R.id.wordle_45)},
+                {findViewById(R.id.wordle_51),findViewById(R.id.wordle_52),findViewById(R.id.wordle_53),findViewById(R.id.wordle_54),findViewById(R.id.wordle_55)},
+                {findViewById(R.id.wordle_61),findViewById(R.id.wordle_62),findViewById(R.id.wordle_63),findViewById(R.id.wordle_64),findViewById(R.id.wordle_65)}
+        };
+        myEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                // showMyDialog();
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d("textChanged len :",Integer.toString(charSequence.length()));
+                Log.d("textChanged i :",Integer.toString(i));
+                Log.d("textChanged i1:",Integer.toString(i1));
+                Log.d("textChanged i2:",Integer.toString(i2));
+                Log.d("textChanged :",charSequence.toString());
+//                Log.d("textChanged seq:",charSequence.toString().substring(i1,i1+1)+"\n++++++\n");
+
+                if(i1<5) {
+                    if(i2<i1) ((TextView)wordls[row_counter][i2]).setText("");
+                    else ((TextView)wordls[row_counter][i1]).setText(charSequence.toString().substring(i1,i1+1));
+                }else{
+                    ((EditText)findViewById(R.id.wordle_et1)).setText(charSequence.subSequence(i,i2));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+        myEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    Log.d("key:::::::","true"+Integer.toString(keyCode));
+                    if (((EditText) findViewById(R.id.wordle_et1)).getText().toString().toUpperCase(Locale.ROOT).equals("AIKYA")){
+                        Intent I = new Intent(v.getContext(),MainActivity.class);
+                        startActivityForResult(I,0);
+                    }
+                    row_counter++;
+                    if(row_counter>=6){
+                        for (int i=0;i<6;i++){
+                            for(int j=0;j<5;j++){
+                                ((EditText)wordls[i][j]).setText("");
+                            }
+                        }
+                        row_counter=0;
+                    }
+                    return true;
+                }
+                return false;
             }
         });
     }
