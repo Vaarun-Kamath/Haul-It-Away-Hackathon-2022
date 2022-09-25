2c58c5b7c0e6e2b76f10a97dfb57b4f1563d889c
VarunKamath <varunkamath05@gmail.com>
2022-09-25 09:38:32 +0530

    test

--- StudioApplication/app/src/main/java/com/example/studioapplication/MainActivity.java
+++ StudioApplication/app/src/main/java/com/example/studioapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.studioapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -9,6 +11,7 @@ import androidx.fragment.app.FragmentTransaction;
 //import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
@@ -21,13 +24,21 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.studioapplication.databinding.ActivityMainBinding;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-
     ActivityMainBinding binding;
     private static int MICROPHONE_PERMISSION_CODE = 200;
+//    Context context = getApplicationContext();
+    private FirebaseDatabase _firebase = FirebaseDatabase.getInstance();
+    DatabaseReference mDatabase = _firebase.getReference("Dtap/");
     MediaRecorder mediaRecorder;
     MediaPlayer mediaPlayer;
 
@@ -35,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+        writeNewUser("422","GG","fg@gmail.com");
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 //        setContentView(R.layout.activity_main); TODO IDK WHAT HE DID BUT HE REMOVED THE PARAMETERS HERE I DIDNT WANT TO DELETE :)
         setContentView(binding.getRoot());
@@ -134,4 +149,61 @@ public class MainActivity extends AppCompatActivity {
         File file = new File(musicDirectory,"testRecordingFile"+".mp3");
         return file.getPath();
     }
+
+    //FIREBASE
+
+    public void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+
+    @IgnoreExtraProperties
+    public class User {
+
+        public String username;
+        public String email;
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public User(String username, String email) {
+            this.username = username;
+            this.email = email;
+        }
+
+    }
+
+   private ChildEventListener childListner = new ChildEventListener() {
+
+       @Override
+       public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+       }
+
+       @Override
+       public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//           btnRecordPressed();
+           Context context = getApplicationContext();
+           CharSequence text = "Hello toast!";
+           int duration = Toast.LENGTH_SHORT;
+           Toast toast = Toast.makeText(context, text, duration);
+           toast.show();
+       }
+
+       @Override
+       public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+       }
+
+       @Override
+       public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+       }
+
+       @Override
+       public void onCancelled(@NonNull DatabaseError error) {
+
+       }
+   };
 }
